acf1d6d71ef3aab5764ae4f727e9a1ce455a9a24
Akseli <s2100140@edu.bc.fi>
2021-09-09 13:10:48 +0300

    proxy and axios calls working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [userName, setUsername] = useState("");
+  useEffect(() => {
+    getNames();
+  }, []);
+
+  const getNames = async () => {
+    const response = await axios.get("/names");
+    console.log(response);
+    setUsername(response.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My awesome CI/CD full starck website </h1>
+      <h3> My name is {userName} </h3>
     </div>
   );
 }
